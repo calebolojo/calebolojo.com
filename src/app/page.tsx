fb303b6b5b16a94b83de14943c3ba11a2f0fc01c
veristein <veristeintech@gmail.com>
2024-03-09 23:05:34 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import * as React from 'react';
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+import { HiChevronDown } from 'react-icons/hi2';
 import '@/lib/env';
 
 /**
@@ -14,28 +18,44 @@ import '../styles/home.scss';
 
 import Header from '../components/Header/Header';
 
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
 export default function HomePage() {
   return (
     <main>
       <Header />
       <section className='home-hero'>
-        <div className='home-hero-container container mx-auto'>
+        <div className='home-hero-container container relative mx-auto'>
           <div className='home-hero-wrap'>
-            <h1 className='font-primary pb-10 font-semibold text-white'>
-              Crafting Robust Full Stack Web Applications and Managing Cloud
-              Infrastructure.
+            <p className='intro pb-[60px] font-semibold uppercase text-white'>
+              Hello. I'm Caleb Olojo
+            </p>
+            <h1 className='font-primary pb-6 font-semibold text-white'>
+              I Craft Robust Full Stack <br />
+              Applications and Orchestrate
+              <br />
+              Cloud Infrastructure.
             </h1>
-            <p className='font-300 font-light text-white'>
-              Hi! I'm Caleb, a full stack engineer proficient in designing and
+            <p className='copy font-300 pt-6 font-light text-white'>
+              A Senior Software Engineer proficient in designing and
               implementing scalable, secure, and high-performance applications
               on AWS Cloud Services. Proven expertise in collaborating with
               cross-functional teams to deliver innovative solutions aligned
               with business objectives.
             </p>
+          </div>
+          <div>
+            <button>
+              <ul>
+                <li>
+                  <HiChevronDown />
+                </li>
+                <li>
+                  <HiChevronDown />
+                </li>
+                <li>
+                  <HiChevronDown />
+                </li>
+              </ul>
+            </button>
           </div>
         </div>
       </section>
