dc22a8eb17f3284366d8c71b36ccf5882d1faf51
veristein <veristeintech@gmail.com>
2024-03-10 19:33:21 -0400

    update

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Caleb Olojo',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'Portfolio Website for Caleb Olojo',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://www.calebolojo.com',
 };
