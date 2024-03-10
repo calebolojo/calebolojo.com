450a5f8c044307f05f54f6d744a4c148e206f5d4
veristein <veristeintech@gmail.com>
2024-03-10 19:35:01 -0400

    update

--- src/app/components/NavLink/NavLink.tsx
+++ src/app/components/NavLink/NavLink.tsx
@@ -8,10 +8,16 @@ Use the exact property to change it to an exact match with the whole URL pathnam
 */
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { ComponentProps } from 'react';
 
 import './NavLink.scss'
+
+interface NavLinkProps extends ComponentProps<'a'> {
+  href: string;
+  exact: boolean;
+}
  
-export const NavLink = ({ href, exact, children, ...props }) => {
+export const NavLink = ({ href, exact, children, ...props }: NavLinkProps) => {
   const pathname = usePathname()
   const active = ' active'
   const isActive = exact ? pathname === href : pathname.startsWith(href)
